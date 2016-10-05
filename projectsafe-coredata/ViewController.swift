99f4b7c30ea52d0fcacc9358c07675e8645db774
noah macri <noahmacri@noahs-macbook-pro.local>
2016-10-05 18:20:02 +0800

    Initial Commit

---
+++ projectsafe-coredata/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  projectsafe-coredata
+//
+//  Created by noah macri on 5/10/2016.
+//  Copyright © 2016 noah macri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
